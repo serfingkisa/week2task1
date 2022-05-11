9c56ad19698dc1a14fcd688e68f566696d0246b9
serfingkisa <serfingkisa@github.com>
2022-05-11 23:18:22 +0800

    first

--- main.js
+++ main.js
@@ -35,17 +35,27 @@ var initHttpServer = () => {
     var app = express();
     app.use(bodyParser.json());
 
+    
     app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain)));
-    app.post('/mineBlock', (req, res) => {
+
+
+    app.get('/mineBlock', (req, res) => {
+
+        console.log(req.body)
+        
         var newBlock = generateNextBlock(req.body.data);
         addBlock(newBlock);
         broadcast(responseLatestMsg());
         console.log('block added: ' + JSON.stringify(newBlock));
         res.send();
     });
+
+
     app.get('/peers', (req, res) => {
         res.send(sockets.map(s => s._socket.remoteAddress + ':' + s._socket.remotePort));
     });
+
+
     app.post('/addPeer', (req, res) => {
         connectToPeers([req.body.peer]);
         res.send();
